e4855cfa1d72a1426028bc68117f594190f3c23c
hachemj <jadd.lhm@gmail.com>
2024-12-27 05:43:39 +0200

    Every button and hyper links are now functioning except Join Now in the Sign Up page

--- app/src/main/java/com/example/jads/ConnectionActivity.java
+++ app/src/main/java/com/example/jads/ConnectionActivity.java
@@ -1,9 +1,15 @@
 package com.example.jads;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ClickableSpan;
+import android.text.style.ForegroundColorSpan;
 import android.view.View;
 import android.widget.Button;
 import android.content.*;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -24,6 +30,34 @@ public class ConnectionActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        TextView registerNowText = findViewById(R.id.textView);
+
+        // Text to display
+        String fullText = "New to La Secte? Register Now >";
+
+        // Create a SpannableString to modify part of the text
+        SpannableString spannableString = new SpannableString(fullText);
+
+        // Apply color to the clickable part
+        int start = fullText.indexOf("Register Now >");
+        int end = start + "Register Now >".length();
+        spannableString.setSpan(new ForegroundColorSpan(Color.BLUE), start, end, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        // Make the clickable part clickable
+        spannableString.setSpan(new ClickableSpan() {
+            @Override
+            public void onClick(View widget) {
+                Intent intent = new Intent(ConnectionActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        }, start, end, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        // Set the text to the TextView
+        registerNowText.setText(spannableString);
+
+        // Enable clicking
+        registerNowText.setMovementMethod(android.text.method.LinkMovementMethod.getInstance());
 
         loginButton = findViewById(R.id.loginButton);
 
--- app/src/main/java/com/example/jads/LoginActivity.java
+++ app/src/main/java/com/example/jads/LoginActivity.java
@@ -1,6 +1,14 @@
 package com.example.jads;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ClickableSpan;
+import android.text.style.ForegroundColorSpan;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +28,27 @@ public class LoginActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        TextView registerNowText = findViewById(R.id.textView);
+
+        String fullText = "Don't have an account? Sign up >";
+
+        SpannableString spannableString = new SpannableString(fullText);
+
+        int start = fullText.indexOf("Sign up >");
+        int end = start + "Sign up >".length();
+        spannableString.setSpan(new ForegroundColorSpan(Color.BLUE), start, end, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        spannableString.setSpan(new ClickableSpan() {
+            @Override
+            public void onClick(View widget) {
+                Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        }, start, end, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        registerNowText.setText(spannableString);
+
+        registerNowText.setMovementMethod(android.text.method.LinkMovementMethod.getInstance());
     }
 }
